831adc183228758c8f90c10ba302f81908f64d75
shalini2203 <83633310+shalini2203@users.noreply.github.com>
2021-09-06 14:15:51 +0000

    Deploying to gh-pages from  @ 0d9d72b5a58703a00cc8cd52b11f586ff2eb8f7b 🚀

---
+++ precache-manifest.2bea8da50736ab37616d510fd9f076f4.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb03ceeeba3a46669776064e02e8ddc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b7864b142760287768c",
+    "url": "/static/css/3.25727338.chunk.css"
+  },
+  {
+    "revision": "fe069b0daa730b848ade",
+    "url": "/static/css/4.a8329f31.chunk.css"
+  },
+  {
+    "revision": "84f979e55b5575893ed7",
+    "url": "/static/css/main.509cd038.chunk.css"
+  },
+  {
+    "revision": "d8ae861b3187f6e3d3e5",
+    "url": "/static/js/2.d7da442c.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.d7da442c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b7864b142760287768c",
+    "url": "/static/js/3.baaf0a33.chunk.js"
+  },
+  {
+    "revision": "fe069b0daa730b848ade",
+    "url": "/static/js/4.6a4b7702.chunk.js"
+  },
+  {
+    "revision": "84f979e55b5575893ed7",
+    "url": "/static/js/main.e2c547af.chunk.js"
+  },
+  {
+    "revision": "7a982d624165aac54047",
+    "url": "/static/js/runtime-main.1205c0ff.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "5809d8b694c959034c81e4624e94d9de",
+    "url": "/static/media/Task2.5809d8b6.png"
+  },
+  {
+    "revision": "d88ae775604a12d70ab99850b8f179ac",
+    "url": "/static/media/Task3.d88ae775.png"
+  },
+  {
+    "revision": "403f634d8647de1dd782ce0e7fe61fda",
+    "url": "/static/media/Task4.403f634d.png"
+  },
+  {
+    "revision": "2596fbbe5e4512e36ccab57935a83d5f",
+    "url": "/static/media/Task5.2596fbbe.png"
+  },
+  {
+    "revision": "7d7540d610bbb353755055be27bab84d",
+    "url": "/static/media/Task7.7d7540d6.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "5d7dfab963e9ac8d652faf66e4fdc41d",
+    "url": "/static/media/harvardLogo.5d7dfab9.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
