0d9d72b5a58703a00cc8cd52b11f586ff2eb8f7b
Shalini <shaani.singh2203@gmail.com>
2021-09-06 18:36:20 +0530

    Deploy to ordinary repo

---
+++ src/containers/loading/Loading.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./loading.scss";
+
+export default function Loading() {
+  return (
+    <div className="centerContent">
+      <div className="sk-circle">
+        <div className="sk-circle1 sk-child"></div>
+        <div className="sk-circle2 sk-child"></div>
+        <div className="sk-circle3 sk-child"></div>
+        <div className="sk-circle4 sk-child"></div>
+    
+      </div>
+    </div>
+  );
+}
